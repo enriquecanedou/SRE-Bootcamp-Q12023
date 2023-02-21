bd4facf1422ff117e6facb2edd64d99c7c7d6bdc
enriquecanedou <enriquecanedo1@gmail.com>
2023-02-20 18:36:09 -0800

    UPDATE: adding first steps for Docker, completing generateToken method

--- auth_api/java/auth_api/src/main/java/com/wizeline/Methods.java
+++ auth_api/java/auth_api/src/main/java/com/wizeline/Methods.java
@@ -1,12 +1,56 @@
 package com.wizeline;
 
+import io.jsonwebtoken.Jwts;
+
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
+import java.time.temporal.ChronoUnit;
 import java.util.Properties;
+import java.util.UUID;
+import java.time.Instant;
+import java.util.Date;
 
 public class Methods {
-  public static String generateToken(String username, String password) {
-    return "test";
+  public static String generateToken(String username, String password) throws SQLException {
+
+    Connection connectionDB = Methods.dataBaseConnection();
+    PreparedStatement prepStatement = connectionDB.prepareStatement("SELECT * FROM bootcamp_tht.users WHERE username=?");
+    prepStatement.setString(1, username.toString());
+    ResultSet rSet = prepStatement.executeQuery();
+    String saltValue = null;
+    String dbPassword = null;
+    String dbRole = null;
+
+    while(rSet.next()){
+      System.out.println("UserName: "+ rSet.getString(1) + " Password: " + rSet.getString(2) + " Salt: " + rSet.getString(3) + " Role: " + rSet.getString(4));
+      dbPassword = rSet.getString(2);
+      saltValue = rSet.getString(3);
+      dbRole = rSet.getString(4);
+    }
+
+    prepStatement.close();
+
+    String pwdToEncrypt = password + saltValue;
+    String pwdEncrypted = encryptThisString(pwdToEncrypt);
+    if (pwdEncrypted.toString().equals(dbPassword)){
+      String jwtToken = Jwts.builder()
+              .claim("userName", username)
+              .claim("role", dbRole)
+              .setSubject(username)
+              .setId(UUID.randomUUID().toString())
+              .setIssuedAt(Date.from(Instant.now()))
+              .setExpiration(Date.from(Instant.now().plus(5l, ChronoUnit.MINUTES)))
+              .compact();
+
+      return jwtToken;
+
+    }else{
+      return "Error, invalid username/password";
+    }
+
   }
   public static String accessData(String authorization){
     return "test";
@@ -46,12 +90,6 @@ public class Methods {
       //load a properties file from class path, inside static method
       prop.load(input);
 
-      //get the property value and print it out, LOGGING Purposes
-      System.out.println(prop.getProperty("connection.driver"));
-      System.out.println(prop.getProperty("connection.url"));
-      System.out.println(prop.getProperty("connection.userName"));
-      System.out.println(prop.getProperty("connection.password"));
-
       //Defining connection values
       String dbDriver = prop.getProperty("connection.driver");
       String dbURL = prop.getProperty("connection.url");
@@ -69,6 +107,37 @@ public class Methods {
     return connection;
   }
 
+  public static String encryptThisString(String input)
+  {
+    try {
+      // getInstance() method is called with algorithm SHA-512
+      MessageDigest md = MessageDigest.getInstance("SHA-512");
+
+      // digest() method is called
+      // to calculate message digest of the input string
+      // returned as array of byte
+      byte[] messageDigest = md.digest(input.getBytes());
+
+      // Convert byte array into signum representation
+      BigInteger no = new BigInteger(1, messageDigest);
+
+      // Convert message digest into hex value
+      String hashtext = no.toString(16);
+
+      // Add preceding 0s to make it 32 bit
+      while (hashtext.length() < 32) {
+        hashtext = "0" + hashtext;
+      }
+
+      // return the HashText
+      return hashtext;
+    }
+
+    // For specifying wrong message digest algorithms
+    catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
 }
 
